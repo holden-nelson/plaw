b37877f82fefc5334610fbc352fe4ca5b6abccec
Holden Nelson <holden.nathaniel.nelson@gmail.com>
2021-04-21 13:45:21 -0600

    add more helper functions and implement account and shop endpoints.

--- plaw.py
+++ plaw.py
@@ -67,17 +67,21 @@ class Plaw():
             Token refreshes
             Converting datetimes to iso format
             Pagination
-            Rate Limiting
+            Rate Limiting (soon)
 
         :param endpoint: string of the endpoint being called.
                          passed on to _call()
         :param params: dict of query parameters used in the api call
-        :return: the decoded JSON from response
+        :return: a generator for each page of the decoded JSON from response
         '''
         if params:
             # look for datetimes to convert and query ops
             for key, param in params.items():
-                # datetimes always passed in as list with query op
+                # datetimes may not have query op passed in
+                if isinstance(param, datetime):
+                    params[key] = param.isoformat()
+
+                # datetimes may be passed in with query op
                 if isinstance(param, list):
                     if isinstance(param[1], datetime):
                         params[key][1] = param[1].isoformat()
@@ -114,25 +118,70 @@ class Plaw():
             else:
                 break
 
-    def test_generator(self):
-        params = {
-
-        }
-        while True:
-            response = self._call_api(
-                'API/Account.json',
-                params)
-            yield response
-
-            if 'offset' in response['@attributes']:
-                count = int(response['@attributes']['count'])
-                offset = int(response['@attributes']['offset'])
-
-                if count - offset > 100:
-                    params['offset'] = str(offset + 100)
-
+    def _strip_attributes(self, response):
+        '''
+        strips attributes info from json response
+        also wraps a list around solo responses
+        # TODO move that functionality to its own function
+        :param response: the decoded json from the api
+        :return: list of dicts from response
+        '''
+        for key in response.keys():
+            if key != '@attributes':
+                # if only one result there will be no list in JSON
+                if isinstance(response[key], dict):
+                    return [response[key]]
                 else:
-                    break
-            else:
-                break
+                    return response[key]
+
+    def _combine_paginated_response(self, paginated_response):
+        '''
+        takes a generator object and returns the full un-paginated response
+        :param paginated_response: the paginated response from _call_api
+        :return: the combined response list
+        '''
+        combined_response = []
+        for page in paginated_response:
+            combined_response += self._strip_attributes(page)
+
+        return combined_response
+
+
+    def account(self):
+        '''
+        interface for the account endpoint
+        :return: dict of account info - accountID and name
+        '''
+        # account is special in that we don't return a list, we just return a dict
+        # and we also remove the 'link' entry
+
+        account_info = self._strip_attributes(next(self._call_api('API/Account.json')))
+        del account_info[0]['link']
+        return account_info[0]
+
+    def shop(self, params=None, paginated=False):
+        '''
+        interface for the shop endpoint
+        :return: depends on paginated flag.
+                 if False (default) returns list of dicts of shop info
+                 if True returns a generator to get results from
+        '''
+        paginated_response = self._call_api(f'API/Account/{self.account_id}/Shop.json',
+                                            params)
+        if paginated:
+            return paginated_response
+        else:
+            return self._combine_paginated_response(paginated_response)
+
+    def employee(self):
+        '''
+        interface for the employee endpoint
+        :return:
+        '''
+
+    def employee_hours(self):
+        '''
+        interface for the employeeHours endpoint
+        :return:
+        '''
 
